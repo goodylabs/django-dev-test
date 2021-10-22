b81bbca3bb8e2ffdec9671427889da53e41d7b72
Sara Kubczak <kubczaksara@gmail.com>
2021-10-22 17:02:10 +0200

    Updated model

--- analytics/models.py
+++ analytics/models.py
@@ -9,3 +9,10 @@ class Event(models.Model):
     name = models.CharField('the name of the event', max_length=255)
     created_at = models.DateTimeField(default=timezone.now)
     additional_data = models.CharField(max_length=300, blank=True, default='')
+
+    class Meta:
+        verbose_name = "event"
+        verbose_name_plural = "events"
+
+    def __str__(self):
+        return f"{self.name}"
