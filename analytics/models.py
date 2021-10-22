3d7536ea91b3e6f7175c4cd1b670da760567f222
Sara Kubczak <kubczaksara@gmail.com>
2021-10-22 16:20:00 +0200

    Created model with given fields

--- analytics/models.py
+++ analytics/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Event(models.Model):
-    pass
+    """
+       Stores information about the event.
+    """
+    name = models.CharField('the name of the event', max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
+    additional_data = models.CharField(max_length=300, blank=True, default='')
