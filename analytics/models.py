29953b0127e07adf3d34dc94f6689f9810076fcb
Pawel Tarasiuk <tpablo@tpablo.net>
2022-09-16 12:15:49 +0200

    Created analytics.models.Event model

--- analytics/models.py
+++ analytics/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 
 class Event(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    additional_data = models.TextField(blank=True, default='')
